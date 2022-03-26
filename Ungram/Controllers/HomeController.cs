f9074954c9d2aa0f35c09edb72277e95b72aa981
Nurzhan <nurzhait@gmail.com>
2022-03-27 01:31:00 +0600

    Add project files.

---
+++ Ungram/Controllers/HomeController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using Ungram.Models;
+using Ungram.Services;
+
+namespace Ungram.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly InstaService _instaService;
+        public HomeController(ILogger<HomeController> logger, InstaService instaService)
+        {
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+            _instaService = instaService ?? throw new ArgumentNullException(nameof(instaService));
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Index([Bind("Username,Password,SaveSession")] LoginViewModel vm)
+        {
+            if (ModelState.IsValid)
+            {
+                var authResult = await _instaService.Login(vm.Username, vm.Password);
+
+                if (authResult.Item1)
+                {
+                    ViewData["login"] = true;
+                }
+
+                var result = await _instaService.GetNotFollowingBack(vm.Username);
+                ViewData["NotFollowingBack"] = result;
+                return View();
+            }
+            return View(vm);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Ungram/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Ungram.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
